33a93b5b3ab1fb81e325036b5ea6279eaf9d611a
johannajonsson <jojo.risin@outlook.com>
2026-06-10 09:39:36 +0200

    delete old unused files. update default properties jwt url. delete unnecessary columndefinition

--- src/main/java/se/jensen/johanna/fakestoreproductservice/mapper/ProductMapper.java
+++ src/main/java/se/jensen/johanna/fakestoreproductservice/mapper/ProductMapper.java
@@ -9,12 +9,8 @@ import se.jensen.johanna.fakestoreproductservice.model.Product;
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-  /*@Mapping(target = "externalId", source = "id")
-  Product toProduct(ProductSyncDTO productSyncDTO);*/
-
   @Mapping(target = "productId", source = "productId")
   ProductDTO toProductDTO(Product product);
-
 
   default Product toProduct(ProductSyncDTO dto) {
     return Product.create(dto.id(), dto.title(), dto.price(), dto.description(), dto.image());
--- src/main/java/se/jensen/johanna/fakestoreproductservice/model/Product.java
+++ src/main/java/se/jensen/johanna/fakestoreproductservice/model/Product.java
@@ -1,7 +1,6 @@
 package se.jensen.johanna.fakestoreproductservice.model;
 
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -34,9 +33,7 @@ public class Product {
   private Long externalId;
   private String title;
   private Integer price;
-  @Column(columnDefinition = "TEXT")
   private String description;
-  @Column(columnDefinition = "TEXT")
   private String image;
   private Instant createdAt;
   private Instant updatedAt;
